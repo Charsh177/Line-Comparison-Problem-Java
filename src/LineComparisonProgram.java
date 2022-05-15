45b9cf0b3a4665f0debeead79b1f445b58c89dd3
Charsh177 <harshchougule177@gmail.com>
2022-05-15 19:20:57 +0530

    ADDED: Check equality of two lines

--- src/LineComparisonProgram.java
+++ src/LineComparisonProgram.java
@@ -21,5 +21,34 @@ public class LineComparisonProgram {
 
         double len_of_line = Math.sqrt(x + y);
         System.out.println("Length of the line is " + len_of_line);
+
+        double len_of_line1 = Math.sqrt(x + y);
+        System.out.println("Length of line 1 is " + len_of_line1);
+
+        System.out.println("Calculation for second line length");
+        System.out.print("Enter x3 co-ordinate: ");
+        int x3 = sc.nextInt();
+        System.out.print("Enter x4 co-ordinate: ");
+        int x4 = sc.nextInt();
+        System.out.print("Enter y3 co-ordinate: ");
+        int y3 = sc.nextInt();
+        System.out.print("Enter y4 co-ordinate: ");
+        int y4 = sc.nextInt();
+
+        double p = Math.pow((x4 - x3), 2);
+        double s = Math.pow((y4 - y3), 2);
+
+        double len_of_line2 = Math.sqrt(p + s);
+        System.out.println("Length of line 2 is " + len_of_line2);
+
+        if (len_of_line1 == len_of_line2)
+            System.out.println("Length are equal");
+        else
+            System.out.println("Length are not equal");
+
+        if (len_of_line1 > len_of_line2)
+            System.out.println("Line 1 is greater than Line 2");
+        else
+            System.out.println("Line 1 is less than Line 2");
     }
 }
