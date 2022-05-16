9c5b947b05049c428fe73d555b4ca764306e6fb7
Charsh177 <harshchougule177@gmail.com>
2022-05-17 00:43:21 +0530

    UPDATED: Calculate length of first line

--- src/LineComparisonProgram.java
+++ src/LineComparisonProgram.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
-
 public class LineComparisonProgram {
     public static void main(String[] args) {
-
-        System.out.println("Welcome to Line Comparison Computation Program");
-
-        // Calculate length of a line
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter x1 co-ordinate: ");
-        int x1 = sc.nextInt();
-        System.out.print("Enter x2 co-ordinate: ");
-        int x2 = sc.nextInt();
-        System.out.print("Enter y1 co-ordinate: ");
-        int y1 = sc.nextInt();
-        System.out.print("Enter y2 co-ordinate: ");
-        int y2 = sc.nextInt();
+        int x1 = 7;
+        int x2 = 4;
+        int y1 = 9;
+        int y2 = 6;
 
         double x = Math.pow((x2 - x1), 2);
         double y = Math.pow((y2 - y1), 2);
 
-        double len_of_line = Math.sqrt(x + y);
-        System.out.println("Length of the line is " + len_of_line);
+        Double len_of_line = Math.floor(Math.sqrt(x + y));
+        System.out.println("Length of the first line " + len_of_line);
     }
 }
